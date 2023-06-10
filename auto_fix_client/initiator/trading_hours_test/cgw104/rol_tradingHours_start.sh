e33faa63744aea95f72753e08f3236b25ab41d83
zhenghuaimao <873516470@qq.com>
2023-06-10 18:05:30 +0900

    change cgw101～cgw104 start_shell path

--- auto_fix_client/initiator/trading_hours_test/cgw104/rol_tradingHours_start.sh
+++ auto_fix_client/initiator/trading_hours_test/cgw104/rol_tradingHours_start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd ./initiator/trading_hours_test/cgw104
+cd /app/data/auto_fix_client/initiator/trading_hours_test/cgw104
 sleep 5
 python3 /app/data/auto_fix_client/initiator/trading_hours_test/cgw104/rol_tradingHours_client.py /app/data/auto_fix_client/initiator/trading_hours_test/cgw104/rol_tradingHours_client.cfg
