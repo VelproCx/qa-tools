fa00cf91426f02e07ec79242ecbe5143479300ca
zhenghuaimao <873516470@qq.com>
2023-06-10 18:05:30 +0900

    change cgw101～cgw104 start_shell path

--- auto_fix_client/initiator/trading_hours_test/cgw101/rol_tradingHours_start.sh
+++ auto_fix_client/initiator/trading_hours_test/cgw101/rol_tradingHours_start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-cd /app/data/FIX_CLIENT/FSX_Client_FIX4.2/initiator/trading_hours_test/cgw101
+cd /app/data/auto_fix_client/initiator/trading_hours_test/cgw101
 #本地调试路径：
 #/initiator/trading_hours_test/cgw101
 #uat调试路径：
-#/app/data/FIX_CLIENT/FSX_Client_FIX4.2/initiator/trading_hours_test/cgw101
+#/app/data/auto_fix_client/initiator/trading_hours_test/cgw101
 sleep 5
-python3 /app/data/FIX_CLIENT/FSX_Client_FIX4.2/initiator/trading_hours_test/cgw101/rol_tradingHours_client.py /app/data/FIX_CLIENT/FSX_Client_FIX4.2/initiator/trading_hours_test/cgw101/rol_tradingHours_client.cfg
+python3 /app/data/auto_fix_client/initiator/trading_hours_test/cgw101/rol_tradingHours_client.py /app/data/auto_fix_client/initiator/trading_hours_test/cgw101/rol_tradingHours_client.cfg
 #本地调试路径：
 #/initiator/trading_hours_test/cgw101/rol_tradingHours_client.py  /initiator/trading_hours_test/cgw101/rol_tradingHours_client.cfg
 #uat调试路径：
-#/app/data/FIX_CLIENT/FSX_Client_FIX4.2/initiator/trading_hours_test/cgw101/rol_tradingHours_client.py  /app/data/FIX_CLIENT/FSX_Client_FIX4.2/initiator/trading_hours_test/cgw101/rol_tradingHours_client.cfg
+#/app/data/auto_fix_client/initiator/trading_hours_test/cgw101/rol_tradingHours_client.py  /app/data/auto_fix_client/initiator/trading_hours_test/cgw101/rol_tradingHours_client.cfg
