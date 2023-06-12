db6e6884192c900e287546a0e64a2e650afae599
陈翔 <1009015490@qq.com>
2023-06-12 16:08:09 +0900

    add rolx_full_stock_test and rex_full_stock_test

---
+++ auto_fix_client/initiator/rex_full_stock_test/cgw105/rex_load_start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd /app/data/auto_fix_client/initiator/rex_full_stock_test/cgw105
+sleep 5
+python3 /app/data/auto_fix_client/initiator/rex_full_stock_test/cgw105/rex_load_client.py /app/data/auto_fix_client/initiator/rex_full_stock_test/cgw105/rex_load_client.cfg
