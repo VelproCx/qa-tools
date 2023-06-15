0adb19e0b571dd86b7dc62d9fb2395d703b7da3c
zhenghuaimao <873516470@qq.com>
2023-06-15 18:55:58 +0900

    update files

--- rolx_fix_client/initiator/rolx_full_stock_test/cgw101/rol_load_start.sh
+++ rolx_fix_client/initiator/rolx_full_stock_test/cgw101/rol_load_start.sh
@@ -1,4 +1,10 @@
+##!/bin/sh
+#cd /app/data/auto_fix_client/initiator/rolx_full_stock_test/cgw101
+#sleep 5
+#python3 /app/data/auto_fix_client/initiator/rolx_full_stock_test/cgw101/rol_load_client.py /app/data/auto_fix_client/initiator/rolx_full_stock_test/cgw101/rol_load_client.cfg
+
+
 #!/bin/sh
-cd /app/data/auto_fix_client/initiator/rolx_full_stock_test/cgw101
+cd /initiator/rolx_full_stock_test/cgw101
 sleep 5
-python3 /app/data/auto_fix_client/initiator/rolx_full_stock_test/cgw101/rol_load_client.py /app/data/auto_fix_client/initiator/rolx_full_stock_test/cgw101/rol_load_client.cfg
+python3 rol_load_client.py rol_load_client.cfg
