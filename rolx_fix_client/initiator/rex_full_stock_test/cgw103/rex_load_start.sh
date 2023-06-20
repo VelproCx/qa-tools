df716802c6594c6cad5f225c97181c4701ee0620
zhenghuaimao <873516470@qq.com>
2023-06-20 17:17:53 +0900

    rolx_fix_client--initiator--regression add msgType check

--- rolx_fix_client/initiator/rex_full_stock_test/cgw103/rex_load_start.sh
+++ rolx_fix_client/initiator/rex_full_stock_test/cgw103/rex_load_start.sh
@@ -1,4 +1,11 @@
+##!/bin/sh
+#cd /app/data/auto_fix_client/initiator/rex_full_stock_test/cgw103
+#sleep 5
+#python3 /app/data/auto_fix_client/initiator/rex_full_stock_test/cgw103/rex_load_client.py /app/data/auto_fix_client/initiator/rex_full_stock_test/cgw103/rex_load_client.cfg
+
+
+
 #!/bin/sh
-cd /app/data/auto_fix_client/initiator/rex_full_stock_test/cgw103
+cd /initiator/rex_full_stock_test/cgw103
 sleep 5
-python3 /app/data/auto_fix_client/initiator/rex_full_stock_test/cgw103/rex_load_client.py /app/data/auto_fix_client/initiator/rex_full_stock_test/cgw103/rex_load_client.cfg
+python3 rex_load_client.py rex_load_client.cfg
