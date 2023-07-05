9bead33d86a04fcb5d81e01b0ce810c4154f86eb
zhenghuaimao <873516470@qq.com>
2023-07-05 12:24:45 +0900

    change text date

--- edp_fix_client/initiator/regression/edp_regression_client.py
+++ edp_fix_client/initiator/regression/edp_regression_client.py
@@ -17,7 +17,7 @@ def main(config_file):
 
         initiator.start()
         application.load_test_case()
-        time.sleep(5)
+        time.sleep(20)
         initiator.stop()
 
     except (quickfix.ConfigError, quickfix.RuntimeError) as e:
