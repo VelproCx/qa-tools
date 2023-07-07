05c2f8d11763758fb3d838266489fcf63c67b637
zhenghuaimao <873516470@qq.com>
2023-07-07 17:21:14 +0900

    add edp_performance_test file

--- edp_fix_client/initiator/edp_performance_test/cgw109/edp_performance_start.sh
+++ edp_fix_client/initiator/edp_performance_test/cgw109/edp_performance_start.sh
@@ -4,7 +4,7 @@
 # python3 /app/data/auto_fix_client/initiator/edp_performance_test/cgw101/rol_load_client.py /app/data/auto_fix_client/initiator/edp_performance_test/cgw101/rol_load_client.cfg
 
 
-cd ./edp_fix_client/initiator/edp_performance_test/cgw101
+cd ./edp_fix_client/initiator/edp_performance_test/cgw109
 sleep 5
 python3 edp_performance_client.py edp_performance_client.cfg
 
--- edp_fix_client/initiator/edp_regression_test/edp_regression_start.sh
+++ edp_fix_client/initiator/edp_regression_test/edp_regression_start.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
-cd /Users/elevenchen/Desktop/qa-tools/gRpc_py
-python3 sit_client.py
-sleep 15
+#cd /Users/zhenghuaimao/Desktop/qa-tools/gRpc_py
+#python3 sit_client.py
+#sleep 15
 
 
-cd /Users/elevenchen/Desktop/qa-tools/edp_fix_client/initiator/edp_regression_test
+#cd /Users/zhenghuaimao/Desktop/qa-tools/edp_fix_client/initiator/edp_regression_test
+#sleep 5
+#python3 edp_regression_client.py edp_regression_client.cfg
+
+
+
+
+
+cd /Users/zhenghuaimao/Desktop/qa-tools/edp_fix_client/initiator/edp_regression_test
 sleep 5
-python3 edp_regression_client.py edp_client.cfg
+python3 edp_regression_client.py edp_regression_client.cfg
