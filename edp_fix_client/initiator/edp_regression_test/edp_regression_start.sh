0118b1fa591215f2306bda7c8a7de6d9eb2b4d96
zhenghuaimao <zhenghuaimao@farsightedyu.com>
2023-07-20 15:32:31 +0900

    update edp regression test compare_field_values()

--- edp_fix_client/initiator/edp_regression_test/edp_regression_start.sh
+++ edp_fix_client/initiator/edp_regression_test/edp_regression_start.sh
@@ -12,11 +12,10 @@
 #python3 edp_regression_client.py edp_regression_client.cfg
 
 #jenKens
-cd /var/jenkins_home/workspace/edp_regression_test/gRpc_py
-python3 sit_regression_client.py
-sleep 10
-
-
+#cd /var/jenkins_home/workspace/edp_regression_test/gRpc_py
+#python3 sit_regression_client.py
+#sleep 10
+#
 cd /var/jenkins_home/workspace/edp_regression_test/edp_fix_client/initiator/edp_regression_test
 sleep 5
 python3 edp_regression_client.py edp_regression_client.cfg
