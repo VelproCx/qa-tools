716d12759e7bf70563cb7729fd14cba2c2db9ce4
zhenghuaimao <zhenghuaimao@farsightedyu.com>
2023-07-25 21:30:24 +0900

    Update full stock insert_order_request()

--- edp_fix_client/initiator/edp_regression_test/edp_regression_start.sh
+++ edp_fix_client/initiator/edp_regression_test/edp_regression_start.sh
@@ -16,9 +16,9 @@
 #python3 sit_regression_client.py
 #sleep 10
 #
-cd /var/jenkins_home/workspace/edp_regression_test/edp_fix_client/initiator/edp_regression_test
-sleep 5
-python3 edp_regression_client.py edp_regression_client.cfg
+#cd /var/jenkins_home/workspace/edp_regression_test/edp_fix_client/initiator/edp_regression_test
+#sleep 5
+#python3 edp_regression_client.py edp_regression_client.cfg
 
 #本地
 #cd /Users/zhenghuaimao/Desktop/qa-tools/gRpc_py
@@ -26,8 +26,8 @@ python3 edp_regression_client.py edp_regression_client.cfg
 #sleep 10
 #
 #
-#cd ./edp_fix_client/initiator/edp_regression_test
-#sleep 5
-#python3 edp_regression_client.py edp_regression_client.cfg
+cd ./initiator/edp_regression_test
+sleep 5
+python3 edp_regression_client.py edp_regression_client.cfg
 
 
