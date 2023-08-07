7c868d08a1ea833b271e73d1a8d7f5a80812565e
陈翔 <xiang.chen@farsightedyu.com>
2023-08-07 17:49:54 +0900

    修改dropcopy配置文件

--- edp_fix_client/initiator/edp_dropcopy/edp_dropcopy_client.py
+++ edp_fix_client/initiator/edp_dropcopy/edp_dropcopy_client.py
@@ -17,15 +17,13 @@ def main(config_file):
         initiator = quickfix.SocketInitiator(application, storefactory, settings, logfactory)
 
         initiator.start()
-        time.sleep(10)
 
-        # application.load_test_case()
-        # # 执行完所有测试用例后等待时间
-        # sleep_duration = timedelta(minutes=2)
-        # end_time = datetime.now() + sleep_duration
-        # while datetime.now() < end_time:
-        #     time.sleep(1)
-        initiator.stop()
+        application.load_test_case()
+        # 执行完所有测试用例后等待时间
+        sleep_duration = timedelta(minutes=60)
+        end_time = datetime.now() + sleep_duration
+        while datetime.now() < end_time:
+            time.sleep(1)
 
     except (quickfix.ConfigError, quickfix.RuntimeError) as e:
         print(e)
