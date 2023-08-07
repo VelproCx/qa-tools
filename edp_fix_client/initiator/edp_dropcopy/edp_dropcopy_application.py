5bac6a821519d2b2bbb63c2a8c2d751fa21d77ea
陈翔 <xiang.chen@farsightedyu.com>
2023-08-07 17:46:03 +0900

    修改dropcopy字段解析

--- edp_fix_client/initiator/edp_dropcopy/edp_dropcopy_application.py
+++ edp_fix_client/initiator/edp_dropcopy/edp_dropcopy_application.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 """FIX Application"""
-import difflib
-import os
-import random
 import quickfix as fix
-import time
 import logging
 from model.logger import setup_logger
 
 __SOH__ = chr(1)
-
 
 # log
 setup_logger('logfix', 'logs/edp_report.log')
@@ -74,55 +69,35 @@ class Application(fix.Application):
             else:
                 logfix.info("(recvMsg) Business Message Error")
         elif msgType == "8":
-            avgPx = message.getField(6)
             clOrdID = message.getField(11)
-            CumQty = message.getField(14)
             execID = message.getField(17)
             execTransType = message.getField(20)
             orderID = message.getField(37)
-            ordStatus = message.getField(39)
             side = message.getField(54)
             symbol = message.getField(55)
-            execType = message.getField(150)
-            leavesQty = message.getField(151)
-            contraBroker = message.getField(375)
-            cashMargin = message.getField(544)
-            primaryLastPx = float(message.getField(8031))
-            primaryBidPx = float(message.getField(8032))
-            primaryAskPx = float(message.getField(8033))
-            OrderClassification = message.getField(8060)
-            crossingPriceType = message.getField(8164)
-            phase = message.getField(8171)
-            SelfTradePreventionId = message.getField(8174)
-            marginTransactionType = message.getField(8214)
 
             if execTransType == "2":
                 execRefID = message.getField(19)
                 lastPx = float(message.getField(31))
                 lastShares = message.getField(32)
-                timeInForce = message.getField(59)
-                minQty = message.getField(110)
                 fsxTransactTime = message.getField(8169)
-                rule80A = message.getField(47)
 
-                if (avgPx, clOrdID, CumQty, execID, orderID, ordStatus, rule80A, side, symbol,
-                    timeInForce, execType, leavesQty, cashMargin,
-                    primaryLastPx, primaryBidPx, primaryAskPx, OrderClassification,
-                    crossingPriceType, fsxTransactTime, SelfTradePreventionId, marginTransactionType, execRefID,
-                    lastPx, lastShares, execTransType, minQty, contraBroker, phase) != '':
+                if (clOrdID, orderID, execID, side, symbol,
+                    fsxTransactTime, execRefID,
+                    lastPx, lastShares, execTransType) != '':
                     logfix.info("(recvMsg) EDP ToSTNeT Confirmation << {}".format(msg))
                 else:
                     logfix.info(
                         "(recvMsg) EDP ToSTNeT Confirmation << {},EDP ToSTNeT Confirmation FixMsg Error!".format(msg))
             else:
+                ordStatus = message.getField(39)
                 text = message.getField(58)
                 origClOrdID = message.getField(41)
                 transactTime = message.getField(60)
-                unknow8165=message.getField(8165)
-                unknow8166=message.getField(8166)
-                if (avgPx, clOrdID, CumQty, execID, orderID, ordStatus, transactTime, side, symbol, transactTime, execType, leavesQty, cashMargin,
-                    primaryLastPx, primaryBidPx, primaryAskPx, OrderClassification,
-                    crossingPriceType, SelfTradePreventionId, marginTransactionType, text,origClOrdID,unknow8165,unknow8166) != "":
+                execType = message.getField(150)
+                if (
+                        clOrdID, execID, orderID, ordStatus, transactTime, side, symbol, transactTime, execType,
+                        text, origClOrdID) != "":
                     logfix.info("(recvMsg) EDP ToSTNeT Rejection << {}".format(msg))
                 else:
                     logfix.info(
@@ -136,4 +111,3 @@ class Application(fix.Application):
         msg = message.toString().replace(__SOH__, "|")
         logfix.info("(Core) R << %s" % msg)
         pass
-
