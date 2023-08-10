8ddee78e1842c856a9cbcda6121531a1cba59faa
zhenghuaimao <zhenghuaimao@farsightedyu.com>
2023-08-10 12:33:14 +0900

    update edp full stock test files

--- edp_fix_client/initiator/edp_full_stock_test/cgw101/edp_fullstock_start.sh
+++ edp_fix_client/initiator/edp_full_stock_test/cgw101/edp_fullstock_start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-#cd /app/data/auto_fix_client/initiator/rolx_full_stock_test/cgw101
+#cd /app/data/qa-tools/edp_fix_client/initiator/edp_full_stock_test/cgw101
 #sleep 5
-#python3 /app/data/auto_fix_client/initiator/rolx_full_stock_test/cgw101/edp_fullstock_client.py /app/data/auto_fix_client/initiator/rolx_full_stock_test/cgw101/edp_fullstock_client.cfg
+#python3 /app/data/qa-tools/edp_fix_client/initiator/edp_full_stock_test/cgw101/edp_fullstock_client.py /app/data/qa-tools/edp_fix_client/initiator/edp_full_stock_test/cgw101/edp_fullstock_client.cfg
 
 
 
