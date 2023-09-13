71304b563f596179bb89b6a01d8070a999bb9b46
zhenghuaimao <zhenghuaimao@farsightedyu.com>
2023-09-13 18:41:40 +0900

    更新performance_test shell脚本

--- edp_fix_client/initiator/edp_performance_test/edp_performance_start.sh
+++ edp_fix_client/initiator/edp_performance_test/edp_performance_start.sh
@@ -6,11 +6,19 @@
 
 sleep 3
 python3 /app/data/qa-tools/edp_fix_client/initiator/edp_performance_test/edp_performance_application.py --account RSIT_EDP_ACCOUNT_1 --Sender RSIT_EDP_1 --Target FSX_SIT_EDP --Host clientgateway101 --Port 5001 &
+sleep 1
 python3 /app/data/qa-tools/edp_fix_client/initiator/edp_performance_test/edp_performance_application.py --account RSIT_EDP_ACCOUNT_1 --Sender RSIT_EDP_2 --Target FSX_SIT_EDP --Host clientgateway102 --Port 5001 &
+sleep 1
 python3 /app/data/qa-tools/edp_fix_client/initiator/edp_performance_test/edp_performance_application.py --account RSIT_EDP_ACCOUNT_1 --Sender RSIT_EDP_3 --Target FSX_SIT_EDP --Host clientgateway103 --Port 5001 &
+sleep 1
 python3 /app/data/qa-tools/edp_fix_client/initiator/edp_performance_test/edp_performance_application.py --account RSIT_EDP_ACCOUNT_1 --Sender RSIT_EDP_4 --Target FSX_SIT_EDP --Host clientgateway104 --Port 5001 &
+sleep 1
 python3 /app/data/qa-tools/edp_fix_client/initiator/edp_performance_test/edp_performance_application.py --account RSIT_EDP_ACCOUNT_1 --Sender RSIT_EDP_5 --Target FSX_SIT_EDP --Host clientgateway105 --Port 5001 &
+sleep 1
 python3 /app/data/qa-tools/edp_fix_client/initiator/edp_performance_test/edp_performance_application.py --account RSIT_EDP_ACCOUNT_1 --Sender RSIT_EDP_6 --Target FSX_SIT_EDP --Host clientgateway106 --Port 5001 &
+sleep 1
 python3 /app/data/qa-tools/edp_fix_client/initiator/edp_performance_test/edp_performance_application.py --account RSIT_EDP_ACCOUNT_1 --Sender RSIT_EDP_7 --Target FSX_SIT_EDP --Host clientgateway107 --Port 5001 &
+sleep 1
 python3 /app/data/qa-tools/edp_fix_client/initiator/edp_performance_test/edp_performance_application.py --account RSIT_EDP_ACCOUNT_1 --Sender RSIT_EDP_8 --Target FSX_SIT_EDP --Host clientgateway108 --Port 5001 &
+sleep 1
 python3 /app/data/qa-tools/edp_fix_client/initiator/edp_performance_test/edp_performance_application.py --account RSIT_EDP_ACCOUNT_1 --Sender RSIT_EDP_9 --Target FSX_SIT_EDP --Host clientgateway109 --Port 5001 &
