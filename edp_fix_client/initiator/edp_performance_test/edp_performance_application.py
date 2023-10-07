b30cf3a0c43b5d0e0a243e605efc98601fd9bdf4
zhenghuaimao <zhenghuaimao@farsightedyu.com>
2023-10-07 17:57:16 +0900

    修改压测脚本中日志生成规则，优化环境变量调用方式

--- edp_fix_client/initiator/edp_performance_test/edp_performance_application.py
+++ edp_fix_client/initiator/edp_performance_test/edp_performance_application.py
@@ -4,8 +4,6 @@
 import argparse
 import configparser
 import sys
-import threading
-
 import quickfix as fix
 import time
 import logging
@@ -16,21 +14,8 @@ import random
 
 __SOH__ = chr(1)
 
-# report
-setup_logger('logfix', 'report.log')
-logfix = logging.getLogger('logfix')
-
-
 class Application(fix.Application):
     execID = 0
-    order_new = 0
-    order_ioc_expired = 0
-    order_accepted = 0
-    order_rejected = 0
-    order_fill_indication = 0
-    order_tostnet_confirmation = 0
-    order_tostnet_rejection = 0
-    order_num = 0
 
     def __init__(self):
         super().__init__()
@@ -40,6 +25,14 @@ class Application(fix.Application):
         self.Target = None
         self.Host = None
         self.Port = None
+
+        self.order_new = 0
+        self.order_ioc_expired = 0
+        self.order_accepted = 0
+        self.order_rejected = 0
+        self.order_fill_indication = 0
+        self.order_tostnet_confirmation = 0
+        self.order_tostnet_rejection = 0
 
     def onCreate(self, sessionID):
         # "服务器启动时候调用此方法创建"
@@ -54,23 +47,10 @@ class Application(fix.Application):
         return
 
     def onLogout(self, sessionID):
-        # "客户端断开连接时候调用此方法"
-        self.logsCheck()
-        # logfix.info(
-        #     "Result: order_new = {}（ order_accepted = {}, order_ioc_expired = {}, order_rejected = {},"
-        #     " order_edp_indication = {}, order_tostnet_confirmation = {}, order_tostnet_rejection = {}".format(
-        #         self.order_new,
-        #         self.order_accepted,
-        #         self.order_ioc_expired,
-        #         self.order_rejected,
-        #         self.order_edp_indication,
-        #         self.order_tostnet_confirmation,
-        #         self.order_tostnet_rejection
-        #     ))
-
-        logfix.info("Result: order_new = {}（ order_accepted = {}, order_rejected = {}）".format(self.order_new,
-                                                                                               self.order_accepted,
-                                                                                               self.order_rejected, ))
+        logfix.info("Result: order_new = {}（ order_accepted = {}, order_rejected = {}）".format(
+            self.order_new,
+            self.order_accepted,
+            self.order_rejected, ))
         logfix.info(
             "Result: order_edp_indication = {}（ order_tostnet_confirmation = {}, order_tostnet_rejection = {}）".format(
                 self.order_fill_indication,
@@ -90,6 +70,7 @@ class Application(fix.Application):
         return
 
     def toApp(self, message, sessionID):
+
         # "发送业务消息时候调用此方法"
         msgtype = message.getHeader().getField(35)
         msg = message.toString().replace(__SOH__, "|")
@@ -132,7 +113,13 @@ class Application(fix.Application):
                 self.order_fill_indication += 1
                 logfix.info("(recvMsg) Order Filled Indication<< {}".format(msg))
         self.onMessage(message, sessionID)
-        return
+        return (
+            self.order_ioc_expired,
+            self.order_accepted,
+            self.order_rejected,
+            self.order_tostnet_confirmation,
+            self.order_tostnet_rejection
+        )
 
     def onMessage(self, message, sessionID):
         """Processing application message here"""
@@ -146,24 +133,19 @@ class Application(fix.Application):
         str1 = ''.join([str(i) for i in random.sample(range(0, 9), 4)])
         return str(t) + str1 + str(self.execID).zfill(6)
 
-    def getOrderQty(self):
-        # 随机生成Qty1-5
-        orderQty = random.randint(1, 5)
-        return orderQty
-
-    def insert_order_request(self, row, account):
+    def insert_order_request(self, row, account, order_num):
         msg = fix.Message()
         header = msg.getHeader()
         header.setField(fix.MsgType(fix.MsgType_NewOrderSingle))
         header.setField(fix.MsgType("D"))
-        # msg.setField(fix.Account("RUAT_EDP_ACCOUNT_1"))
         msg.setField(fix.Account(account))
         msg.setField(fix.ClOrdID(self.getClOrdID()))
         msg.setField(fix.OrderQty(100))
         msg.setField(fix.OrdType("1"))
         msg.setField(fix.Symbol(row["Symbol"]))
+        # msg.setField(fix.Side(row["Side"]))
 
-        if (self.order_num % 2) == 0:
+        if (order_num % 2) == 0:
             msg.setField(fix.Side("2"))
         else:
             msg.setField(fix.Side("1"))
@@ -177,24 +159,17 @@ class Application(fix.Application):
         return msg
 
     def load_test_case(self, account):
+        order_num = 0
         """Run"""
-        with open('../../../testcases/', 'r') as f_json:
+        with open('../../testcases/topix400_case.json', 'r') as f_json:
             case_data_list = json.load(f_json)
             time.sleep(2)
             # 循环所有用例，并把每条用例放入runTestCase方法中，
-            while self.order_num < 2:
-                self.order_num += 1
+            while order_num < 125:
+                order_num += 1
                 for row in case_data_list["testCase"]:
-                    self.insert_order_request(row, account)
-                    time.sleep(0.0035)
-
-    def gen_thread(self, account):
-        threads = []
-        for _ in range(5):
-            t = threading.Thread(target=self.load_test_case(account))
-            threads.append(t)
-        for t in threads:
-            t.start()
+                    self.insert_order_request(row, account, order_num)
+                    time.sleep(0.0025)
 
     def read_config(self, Sender, Target, Host, Port):
         # 读取并修改配置文件
@@ -210,6 +185,7 @@ class Application(fix.Application):
 
 
 def main():
+    global logfix
     try:
         # 使用argparse的add_argument方法进行传参
         parser = argparse.ArgumentParser()  # 创建对象
@@ -226,6 +202,10 @@ def main():
         Host = args.Host
         Port = args.Port
 
+        # report
+        setup_logger('logfix', '{}_Report.log'.format(account))
+        logfix = logging.getLogger('logfix')
+
         cfg = Application()
         cfg.Sender = Sender
         cfg.Target = Target
@@ -236,13 +216,13 @@ def main():
         settings = fix.SessionSettings("edp_performance_client.cfg")
         application = Application()
         application.account = account
-        storefactory = fix.FileStoreFactory(settings)
-        logfactory = fix.FileLogFactory(settings)
-        initiator = fix.SocketInitiator(application, storefactory, settings, logfactory)
+        store_factory = fix.FileStoreFactory(settings)
+        log_factory = fix.FileLogFactory(settings)
+        initiator = fix.SocketInitiator(application, store_factory, settings, log_factory)
 
         initiator.start()
-        application.gen_thread(account)
-        sleep_duration = timedelta(minutes=10)
+        application.load_test_case(account)
+        sleep_duration = timedelta(minutes=5)
         end_time = datetime.now() + sleep_duration
         while datetime.now() < end_time:
             time.sleep(1)
