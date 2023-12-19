2113948399857bda891715b28deaa361831dffa5
xiang-chen-fsy-fsx <xiang.chen.fsy@finstadiumx.co.jp>
2023-12-19 13:24:39 +0900

    update: test ssh key

--- edp_fix_client/initiator/edp_eod/run.sh
+++ edp_fix_client/initiator/edp_eod/run.sh
@@ -8,8 +8,8 @@ sleep 30
 python3 /Users/elevenchen/Desktop/qa-tools/edp_fix_client/initiator/edp_eod/edp_eod.py -f EDP_HRT_Order
 sleep 30
 python3 /Users/elevenchen/Desktop/qa-tools/edp_fix_client/initiator/edp_eod/edp_eod.py -f EDP_RSec_MarketData
-sleep 30
-python3 /Users/elevenchen/Desktop/qa-tools/edp_fix_client/initiator/edp_eod/edp_eod.py -f EDP_RSec_Trade
+#sleep 30
+#python3 /Users/elevenchen/Desktop/qa-tools/edp_fix_client/initiator/edp_eod/edp_eod.py -f EDP_RSec_Trade
 #sleep 1
 #python3 /Users/elevenchen/Desktop/qa-tools/edp_fix_client/initiator/edp_eod/edp_eod.py
 #sleep 1
