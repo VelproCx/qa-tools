52fdfc4510378f79ab564f397026aeb3cb0865f5
xiang-chen-fsy-fsx <xiang.chen.fsy@finstadiumx.co.jp>
2023-12-20 12:05:26 +0900

    perf: 优化性能，取消使用全局变量

--- rolx_fix_client/initiator/rolx_full_account_smoke/full_account_smoke.py
+++ rolx_fix_client/initiator/rolx_full_account_smoke/full_account_smoke.py
@@ -13,24 +13,26 @@ from datetime import datetime
 from model.logger import setup_logger
 import random
 
-__SOH__ = chr(1)
-
-symbols = []
-
 
 class Application(fix.Application):
-    execID = 0
-    order_new = 0
-    order_expired = 0
-    order_accepted = 0
-    order_rejected = 0
-    order_fill_indication = 0
-    order_num = 0
-    order_book_is_close = 0
 
-    def __init__(self):
+    def __init__(self, account, time_of_running, logger):
         super().__init__()
         self.sessionID = None
+        self.account = account
+        self.time_of_running = time_of_running
+        self.logger = logger
+
+        # 定义变量
+        self.execID = 0
+        self.order_new = 0
+        self.order_expired = 0
+        self.order_accepted = 0
+        self.order_rejected = 0
+        self.order_fill_indication = 0
+        self.order_num = 0
+        self.order_book_is_close = 0
+        self.__SOH__ = chr(1)
 
     def onCreate(self, sessionID):
         # "服务器启动时候调用此方法创建"
@@ -46,58 +48,59 @@ class Application(fix.Application):
 
     def onLogout(self, sessionID):
         # "客户端断开连接时候调用此方法"
-        logger.info("Result: order_new = {}（ order_accepted = {}, order_rejected = {}, order_book_is_close ={}）".format(
-            self.order_new,
-            self.order_accepted,
-            self.order_rejected,
-            self.order_book_is_close))
-        logger.info(
+        self.logger.info(
+            "Result: order_new = {}（ order_accepted = {}, order_rejected = {}, order_book_is_close ={}）".format(
+                self.order_new,
+                self.order_accepted,
+                self.order_rejected,
+                self.order_book_is_close))
+        self.logger.info(
             f"Result: order_edp_indication = {self.order_fill_indication}")
 
-        logger.info(f"Result: order_expired = {self.order_expired}")
+        self.logger.info(f"Result: order_expired = {self.order_expired}")
 
         print(f"Session ({sessionID.toString()}) logout !")
         return
 
     def toAdmin(self, message, sessionID):
         # "发送会话消息时候调用此方法"
-        msg = message.toString().replace(__SOH__, "|")
-        logger.info(f"(Core) S >> {msg}")
+        msg = message.toString().replace(self.__SOH__, "|")
+        self.logger.info(f"(Core) S >> {msg}")
         return
 
     def toApp(self, message, sessionID):
         # "发送业务消息时候调用此方法"
         msgType = message.getHeader().getField(35)
-        msg = message.toString().replace(__SOH__, "|")
-        logger.info(f"(sendMsg) New Ack >> {msg}")
+        msg = message.toString().replace(self.__SOH__, "|")
+        self.logger.info(f"(sendMsg) New Ack >> {msg}")
         if msgType == "D":
             self.order_new += 1
         return
 
     def fromAdmin(self, message, sessionID):
         # "接收会话类型消息时调用此方法"
-        msg = message.toString().replace(__SOH__, "|")
-        logger.info(f"(Core) R << {msg}")
+        msg = message.toString().replace(self.__SOH__, "|")
+        self.logger.info(f"(Core) R << {msg}")
         return
 
     def fromApp(self, message, sessionID):
         # "接收业务消息时调用此方法"
         # 使用quickFix框架getField方法提取clOrdId、ordStatus
         ordStatus = message.getField(39)
-        msg = message.toString().replace(__SOH__, "|")
+        msg = message.toString().replace(self.__SOH__, "|")
 
         if ordStatus == "0":
             self.order_accepted += 1
-            logger.info(f"(recvMsg) Order Accepted << {msg}")
+            self.logger.info(f"(recvMsg) Order Accepted << {msg}")
         elif ordStatus == "8":
             self.order_rejected += 1
-            logger.info(f"(recvMsg) Order Rejected << {msg}")
+            self.logger.info(f"(recvMsg) Order Rejected << {msg}")
         elif ordStatus == "4":
             self.order_expired += 1
-            logger.info(f"(recvMsg) Order Expired End Of Day << {msg}")
+            self.logger.info(f"(recvMsg) Order Expired End Of Day << {msg}")
         elif ordStatus == "1" or ordStatus == "2":
             self.order_fill_indication += 1
-            logger.info(f"(recvMsg) Order Filled Indication<< {msg}")
+            self.logger.info(f"(recvMsg) Order Filled Indication<< {msg}")
         self.onMessage(message, sessionID)
         return
 
@@ -118,7 +121,7 @@ class Application(fix.Application):
         header = msg.getHeader()
         header.setField(fix.MsgType(fix.MsgType_NewOrderSingle))
         header.setField(fix.MsgType("D"))
-        msg.setField(fix.Account(account))
+        msg.setField(fix.Account(self.account))
         msg.setField(fix.ClOrdID(self.gen_client_order_id()))
         msg.setField(fix.OrderQty(row["OrderQty"]))
         msg.setField(fix.OrdType("1"))
@@ -128,16 +131,15 @@ class Application(fix.Application):
             msg.setField(fix.Price(row["Price"]))  # 如果值是2，表示是限价单，要设置价格
 
         # 获取TransactTime
-        trstime = fix.TransactTime()
-        trstime.setString(datetime.utcnow().strftime("%Y%m%d-%H:%M:%S.%f"))
-        msg.setField(trstime)
+        transactionTime = fix.TransactTime()
+        transactionTime.setString(datetime.utcnow().strftime("%Y%m%d-%H:%M:%S.%f"))
+        msg.setField(transactionTime)
 
         fix.Session.sendToTarget(msg, self.sessionID)
         return msg
 
     def load_test_case(self):
         """Run"""
-
         with open('smoke_case.json', 'r') as f_json:
             case = json.load(f_json)
             time.sleep(1)
@@ -148,20 +150,18 @@ class Application(fix.Application):
         # 读取并修改配置文件
         config = configparser.ConfigParser(allow_no_value=True)
         config.optionxform = str  # 保持键的大小写
-        config.read('/app/data/qa-tools/rolx_fix_client/initiator/full_of_smoke/full_account_smoke.cfg')
+        config.read('full_account_smoke.cfg')
         config.set('SESSION', 'SenderCompID', sender)
         config.set('SESSION', 'TargetCompID', target)
         config.set('SESSION', 'SocketConnectHost', host)
         config.set('SESSION', 'SocketConnectPort', port)
 
-        with open('/app/data/qa-tools/rolx_fix_client/initiator/full_of_smoke/full_account_smoke.cfg',
+        with open('full_account_smoke.cfg',
                   'w') as configfile:
             config.write(configfile)
 
 
 def main():
-    global account
-    global time_of_running
     try:
         # 使用argparse的add_argument方法进行传参
         parser = argparse.ArgumentParser()  # 创建对象
@@ -193,14 +193,12 @@ def main():
         cfg.Port = port
         cfg.read_config(sender, target, host, port)
 
-        global logger
         # report
         setup_logger('logfix', '{}_report.log'.format(account))
         logger = logging.getLogger('logfix')
 
-        settings = fix.SessionSettings(
-            "/app/data/qa-tools/rolx_fix_client/initiator/full_of_smoke/full_account_smoke.cfg")
-        application = Application()
+        settings = fix.SessionSettings("full_account_smoke.cfg")
+        application = Application(args.account, time_of_running, logger)
         application.account = account
         store_factory = fix.FileStoreFactory(settings)
         log_factory = fix.FileLogFactory(settings)
