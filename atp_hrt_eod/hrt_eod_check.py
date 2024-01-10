7d0647f923c7e21e1da3b9d8ce4808736655c392
huaimao-zheng-fsy-fsx <149136426+huaimao-zheng-fsy-fsx@users.noreply.github.com>
2024-01-10 17:00:47 +0900

    修改：新增ROL_HRT_EOD解析部分

--- atp_hrt_eod/hrt_eod_check.py
+++ atp_hrt_eod/hrt_eod_check.py
@@ -6,48 +6,82 @@ __SOH__ = chr(1)
 
 
 class AtpEod:
-    order_num_acc = 0
-    order_num_rej = 0
-    order_num_exc = 0
-    order_num_fill = 0
-    order_datas_list = []
+    edp_order_num_acc = 0
+    edp_order_num_rej = 0
+    edp_order_num_exc = 0
+    edp_order_num_fill = 0
+    edp_order_datas_list = []
     trade_num = 0
     trade_datas_list = []
 
+    rol_order_num_acc = 0
+    rol_order_num_rej = 0
+    rol_order_num_exc = 0
+    rol_order_num_fill = 0
+    rol_order_datas_list = []
+
     @classmethod
     def sum_eod_num(cls):
-        with open("logs/fix_terminal2_20231212second.log", "r") as file:
+        with open("logs/fix_terminal2_20240109.log", "r") as file:
             for line in tqdm(file):
                 if "send" in line and "35=8" in line:
-                    if "56=HRT_UAT_EDP_D_" in line:
-                        if "49=s_t" in line:
-                            if "39=8" in line:
-                                cls.order_num_rej += 1
-                                order_data = re.sub(r"\x01", "|", line)
-                                # print(order_data)
-                                cls.order_datas_list.append(order_data)
-                            if "39=4" in line:
-                                cls.order_num_exc += 1
-                                order_data = re.sub(r"\x01", "|", line)
-                                # print(order_data)
-                                cls.order_datas_list.append(order_data)
-                            if "39=2" in line or "39=1":
-                                cls.order_num_fill += 1
-                                order_data = re.sub(r"\x01", "|", line)
-                                # print(order_data)
-                                cls.order_datas_list.append(order_data)
-                            # if "39=0" in line:
-                            #     cls.order_num_acc += 1
-                            #     order_data = re.sub(r"\x01", "|", line)
-                            #     # print(order_data)
-                            #     cls.order_datas_list.append(order_data)
-                            cls.trade_num += 1
-                            trade_data = re.sub(r"\x01", "|", line)
-                            cls.trade_datas_list.append(trade_data)
-                else:
-                    pass
-        # print("Order: {}".format(cls.order_num), "Trade: {}".format(cls.trade_num))
-        print(cls.order_num_rej, cls.order_num_exc, cls.order_num_fill, cls.order_num_acc)
+                    if "56=HRT_UAT_EDP_D_" in line and "49=s_t" in line:
+
+                        if "39=8" in line:
+                            cls.edp_order_num_rej += 1
+                            order_data = re.sub(r"\x01", "|", line)
+                            # print(order_data)
+                            cls.edp_order_datas_list.append(order_data)
+                        if "39=4" in line:
+                            cls.edp_order_num_exc += 1
+                            order_data = re.sub(r"\x01", "|", line)
+                            # print(order_data)
+                            cls.edp_order_datas_list.append(order_data)
+                        if "39=2" in line or "39=1":
+                            cls.edp_order_num_fill += 1
+                            order_data = re.sub(r"\x01", "|", line)
+                            # print(order_data)
+                            cls.edp_order_datas_list.append(order_data)
+                        # if "39=0" in line:
+                        #     cls.order_num_acc += 1
+                        #     order_data = re.sub(r"\x01", "|", line)
+                        #     # print(order_data)
+                        #     cls.order_datas_list.append(order_data)
+                        cls.trade_num += 1
+                        trade_data = re.sub(r"\x01", "|", line)
+                        cls.trade_datas_list.append(trade_data)
+
+
+                elif "56=HRT_SIT_ROL_D" in line and "49=s_t" in line:
+                    if "39=8" in line:
+                        cls.rol_order_num_rej += 1
+                        order_data = re.sub(r"\x01", "|", line)
+                        # print(order_data)
+                        cls.rol_order_datas_list.append(order_data)
+                    if "39=4" in line:
+                        cls.rol_order_num_exc += 1
+                        order_data = re.sub(r"\x01", "|", line)
+                        # print(order_data)
+                        cls.rol_order_datas_list.append(order_data)
+                    if "39=2" in line or "39=1":
+                        cls.rol_order_num_fill += 1
+                        order_data = re.sub(r"\x01", "|", line)
+                        print(order_data)
+                        cls.rol_order_datas_list.append(order_data)
+                    if "39=0" in line:
+                        cls.edp_order_num_acc += 1
+                        order_data = re.sub(r"\x01", "|", line)
+                        # print(order_data)
+                        cls.rol_order_datas_list.append(order_data)
+                    cls.trade_num += 1
+                    trade_data = re.sub(r"\x01", "|", line)
+                    cls.trade_datas_list.append(trade_data)
+
+            print(f"rol Eod check:rej: {cls.rol_order_num_rej}, exe: {cls.rol_order_num_exc}, "
+                  f"fill: {cls.rol_order_num_fill}, acc: {cls.rol_order_num_acc}")
+            print(f"rol Eod check:rej: {cls.edp_order_num_rej}, exe: {cls.edp_order_num_exc}, "
+                  f"fill: {cls.edp_order_num_fill}, acc: {cls.edp_order_num_acc}")
+
 
     @classmethod
     def generate_hrt_order_eod(cls):
